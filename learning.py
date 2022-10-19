bc380a242a962a1c0f01afdc07d1dcfeab9448e0
bjrtx <57685415+bjrtx@users.noreply.github.com>
2022-10-19 21:56:24 +0200

    Update learning.py

--- learning.py
+++ learning.py
@@ -1,1 +1,12 @@
+import numpy
+from sklearn.cluster import KMeans
 
+# You need to be in the directory containing those files.
+for fname in (
+    "OUT_above_TH_by_chain_dom1.csv", 
+    "OUT_above_TH_by_chain_comb.csv",
+    "OUT_sum_by_chain_dom1.csv",
+    "OUT_sum_by_chain_comb.csv"
+):
+    data = numpy.loadtxt(fname, delimiter=",").transpose()
+    print(KMeans(n_clusters=2).fit(data).labels_)
