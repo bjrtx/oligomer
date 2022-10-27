0682b7b06e01d597e8d502b5796b498bebee61da
bjrtx <57685415+bjrtx@users.noreply.github.com>
2022-10-27 22:56:40 +0300

    Update learning.py

--- learning.py
+++ learning.py
@@ -1,11 +1,13 @@
 import numpy
 import matplotlib.pyplot as plt
+import string
+import itertools
 
 from sklearn.cluster import KMeans, SpectralClustering
 from sklearn.decomposition import PCA
 
 # You need to be in the directory containing those files.
-index = 0
+plot_index = itertools.count(1)
 plt.subplots(layout="constrained")
 
 for fname in (
@@ -15,23 +17,24 @@ for fname in (
     "OUT_sum_by_chain_comb.csv",
 ):
     data = numpy.loadtxt(fname, delimiter=",").transpose()
-    chain_names = [chr(ord("A") + i) for i in range(24)]
-    for method, name in zip((KMeans, SpectralClustering), ("k-means", "spectral")):
+    chain_names = string.ascii_uppercase[:24] # A to X
+    pca = PCA(n_components=2).fit(data)
+    reduced = pca.transform(data)
+
+    for method, name in zip(
+        (KMeans, SpectralClustering), ("k-means", "spectral")
+    ):
         labels = method(n_clusters=2).fit(data).labels_
         # by convention, the first data point will always be in cluster 0
         if labels[0]:
             labels = 1 - labels
-        print(labels)
-        pca = PCA(n_components=2).fit(data)
-        reduced = pca.transform(data)
         colors = [("navy", "darkorange")[l] for l in labels]
-        index += 1
-        plt.subplot(3, 3, index)
+        plt.subplot(3, 3, next(plot_index))
         plt.scatter(reduced[:, 0], reduced[:, 1], color=colors, alpha=0.8)
         plt.title(
             f"""Data: {fname}.
             Clustering: {name}.
-            Var. expl. by PCA: {sum(pca.explained_variance_ratio_):.1%}."""
+            Var. expl. by 2-D PCA: {sum(pca.explained_variance_ratio_):.1%}."""
         )
         for chain, xy in zip(chain_names, reduced):
             plt.annotate(chain, xy)
