22e2bda3607e763d24965d7c9958f73e31f0ab9f
bjrtx <57685415+bjrtx@users.noreply.github.com>
2022-10-28 15:23:30 +0300

    Update learning.py

--- learning.py
+++ learning.py
@@ -10,17 +10,16 @@ from sklearn.decomposition import PCA
 plot_index = itertools.count(1)
 plt.subplots(layout="constrained")
 
-for fname in (
+for filename in (
     "OUT_above_TH_by_chain_dom1.csv",
     "OUT_above_TH_by_chain_comb.csv",
     "OUT_sum_by_chain_dom1.csv",
     "OUT_sum_by_chain_comb.csv",
 ):
-    data = numpy.loadtxt(fname, delimiter=",").transpose()
-    chain_names = string.ascii_uppercase[:24]  # A to X
+    data = numpy.loadtxt(filename, delimiter=",").transpose()
+    chain_names = string.ascii_uppercase[:24]  # list of letters A to X inclusive
     pca = PCA(n_components=2).fit(data)
     reduced = pca.transform(data)
-
     for method, name in zip((KMeans, SpectralClustering), ("k-means", "spectral")):
         labels = method(n_clusters=2).fit(data).labels_
         # by convention, the first data point will always be in cluster 0
@@ -29,12 +28,16 @@ for fname in (
         colors = [("navy", "darkorange")[l] for l in labels]
         plt.subplot(3, 3, next(plot_index))
         plt.scatter(reduced[:, 0], reduced[:, 1], color=colors, alpha=0.8)
-        plt.title(
-            f"""Data: {fname}.
-            Clustering: {name}.
-            Var. expl. by 2-D PCA: {sum(pca.explained_variance_ratio_):.1%}."""
-        )
         for chain, xy in zip(chain_names, reduced):
-            plt.annotate(chain, xy)
+            plt.annotate(chain, xy, textcoords="offset points", xytext=(3,3))
+        plt.title(
+            f"""
+            Data: {filename.split(".")[0]}.
+            Clustering: {name}.
+            Var. expl. by 2-D PCA: {sum(pca.explained_variance_ratio_):.1%}.
+            """,
+            loc='left'
+        )
+
 
 plt.show()
