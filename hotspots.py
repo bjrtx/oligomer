215c9d5273736f6b7ca9092e163fdd3a30e25334
bjrtx <57685415+bjrtx@users.noreply.github.com>
2022-11-08 00:48:46 +0200

    add simulation.py

--- hotspots.py
+++ hotspots.py
@@ -8,11 +8,17 @@ import mrcfile
 
 
 def read_mrc(filename: str) -> np.ndarray:
+    """
+    Read an MRC map and return a Numpy multidimensional array.
+    """
     with mrcfile.open(filename) as mrc:
         return mrc.data
 
 
 def gloves(hotspot: dict) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Compute the two gloves associated with a given hotspot.
+    """
     try:
         bfr1_glove = read_mrc(hotspot["Bfr1_file_name"]) > float(
             hotspot["Bfr1_Molmap_TH"]
@@ -28,8 +34,10 @@ def gloves(hotspot: dict) -> tuple[np.ndarray, np.ndarray]:
         bfr2_glove = 0
     return bfr1_glove, bfr2_glove
 
-
-def biggest_blob(logical: "np.ndarray[bool]") -> "np.ndarray[bool]":
+def biggest_blob(logical : 'np.ndarray[bool]') -> 'np.ndarray[bool]':
+    """
+    Given a logical multidimensional array, find the largest connected region and return it as a logical array.
+    """
     # labeled is an ndarray of the same size with each pixel labeled by its connected component
     labeled = skimage.measure.label(logical)
     regions = skimage.measure.regionprops(labeled)
@@ -48,28 +56,24 @@ def process(
     with open(hotspot_filename) as hotspot_file:
         hotspots = list(csv.DictReader(hotspot_file))
 
-    chains = (
-        read_mrc(f"Bfr_molmap_chain{char}_res5.mrc")
-        for char in string.ascii_uppercase[:24]
-    )
-
     TH_chains = 0.42
-    logical_chains = [chain > TH_chains for chain in chains]
+    chains = [
+        read_mrc(f"Bfr_molmap_chain{char}_res5.mrc") > TH_chains
+        for char in string.ascii_uppercase[:24]
+    ]
 
     for filename, threshold in zip(map_filenames, map_thresholds):
         map = read_mrc(filename)
-        out = np.empty([len(hotspots), len(logical_chains)])
+        out = np.empty([len(hotspots), len(chains)])
         for i, hotspot in enumerate(hotspots):
             bfr1_glove, bfr2_glove = gloves(hotspot)
-            for j, chain in enumerate(logical_chains):
+            for j, chain in enumerate(chains):
                 # biggest_blob is used to avoid parts of hotspots coming from other chains
                 bfr1_spot = biggest_blob(bfr1_glove & chain)
                 bfr2_spot = biggest_blob(bfr2_glove & chain)
-                comb_size = np.sum(bfr1_spot ^ bfr2_spot)
-                output = (np.sum(map[bfr1_spot]) - np.sum(map[bfr2_spot])) / comb_size
-                print(
-                    f"hotspot {i + 1} chain {string.ascii_uppercase[j]} comb. value {output}"
-                )
+                comb_size = (bfr1_spot ^ bfr2_spot).sum()
+                output = (map.sum(where=bfr1_spot) - map.sum(where=bfr2_spot)) / comb_size
+                print(f"hotspot {i + 1} chain {string.ascii_uppercase[j]} comb. value {output}")
                 out[i, j] = output
 
     return out
