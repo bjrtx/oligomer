72e84c34714f37856c5e3c2c1ce11ff1527f12a0
bjrtx <57685415+bjrtx@users.noreply.github.com>
2022-11-08 14:12:58 +0200

    Update learning.py

--- learning.py
+++ learning.py
@@ -8,17 +8,11 @@ from sklearn.cluster import KMeans, SpectralClustering
 from sklearn.decomposition import PCA
 from sklearn.mixture import GaussianMixture
 
-plot_index = itertools.count(1)
-plt.subplots(layout="constrained")
+
 
 # You need to be in the directory containing those files.
-filenames = (
-    "OUT_above_TH_by_chain_dom1.csv",
-    "OUT_above_TH_by_chain_comb.csv",
-    "OUT_sum_by_chain_dom1.csv",
-    "OUT_sum_by_chain_comb.csv",
-)
-dim = 1 + math.isqrt(3 * len(filenames) - 1)
+
+dim = 3 # arbitrary
 shuffler = [
     (0, 16),
     (1, 14),
@@ -36,6 +30,7 @@ shuffler = [
 
 
 def analyze(data, group_data=True):
+    plot_index = itertools.count(1)
     chain_names = string.ascii_uppercase[:24]  # list of letters A to X inclusive
     if group_data:
         first, second = zip(*shuffler)
@@ -46,6 +41,7 @@ def analyze(data, group_data=True):
     pca = PCA(n_components=2).fit(data)
     reduced = pca.transform(data)
     color_range = ("darkorange", "navy", "green")
+    plt.subplots(layout="constrained")
     # for method, name in zip((KMeans, SpectralClustering), ("k-means", "spectral")):
     for method, name in zip((KMeans,), ("k-means",)):
         labels = method(n_clusters=2).fit(data).labels_
@@ -88,6 +84,13 @@ def analyze(data, group_data=True):
 
 
 if __name__ == "__main__":
+    filenames = (
+    "OUT_above_TH_by_chain_dom1.csv",
+    "OUT_above_TH_by_chain_comb.csv",
+    "OUT_sum_by_chain_dom1.csv",
+    "OUT_sum_by_chain_comb.csv",
+    )
+    dim = 1 + math.isqrt(3 * len(filenames) - 1)
     for filename in filenames:
         try:
             data = numpy.loadtxt(filename, delimiter=",").transpose()
--- simulation.py
+++ simulation.py
@@ -2,15 +2,7 @@ import chimera
 
 from chimera import runCommand as rc
 
-# Read the txt file of the chimera commands in the format of the arrangements doc
-with open("path/chimera_commands.txt") as f:
-    lines = f.readlines()
-
-Bfr1_lines = lines[1::3]
-Bfr2_lines = lines[2::3]
-
-basename = "Structure6-6"  # set a base name as you want
-path = "C:/Users/danist/Desktop/not_drive/Hotspots_284/simulations/"
+# String formats are all wrong because this is Python2.7
 
 # do we really want dimers?
 # pylint: disable-next=invalid-name
@@ -31,15 +23,23 @@ def to_Chimera(blue):
         6: "qa",
     }
 
-    blue_line = f"sel #1:.{':.'.join(d[b] for b in blue)}" if blue else ""
+    blue_line = "sel #1:.{}".format(':.'.join(d[b] for b in blue)) if blue else ""
     red_line = (
-        f"sel #2:.{':.'.join(v for i, v in d.items() if i not in blue)}"
+        "sel #2:.{}".format(':.'.join(v for i, v in d.items() if i not in blue))
         if len(blue) < 12
         else ""
     )
 
 
 def generate(Bfr1_lines, Bfr2_lines, basename, path):
+    map_name = "284postprocess.mrc"
+    Bfr1_pdb = "Bfr1_284_raz-rerefined-correction.pdb"
+    Bfr2_pdb = "Bfr2_284_raz-rerefined-correction.pdb"
+    rc("close session")
+    rc("open " + map_name)
+    rc("open " + Bfr1_pdb)
+    rc("open " + Bfr2_pdb)
+    rc("volume all step 1") # not sure if useful outside graphical display?
     for z, (line_Bfr1, line_Bfr2) in enumerate(zip(Bfr1_lines, Bfr2_lines), start=1):
         rc(line_Bfr1)
         rc("write selected #1 path/sel_Bfr1.pdb")
@@ -49,17 +49,28 @@ def generate(Bfr1_lines, Bfr2_lines, basename, path):
         rc("~select")
         rc("open #31 path/sel_Bfr1.pdb")
         rc("open #32 path/sel_Bfr2.pdb")
-        new_name = f"{basename}_{z}"  # add the basename numbers from 1 to the number of structures
+        new_name = "{}_{}".format(basename, z)  # add the basename numbers from 1 to the number of structures
         rc(
-            f"combine #31-#32 modelId #33 name {new_name}"
+            "combine #31-#32 modelId #33 name {}".format(new_name)
         )  # combine the selections of Bfr1 and Bfr2
-        rc(f"write #33 {path}{new_name}.pdb")  # saves the combined model as pdb
+        rc("write #33 {}{}.pdb".format(path, new_name))  # saves the combined model as pdb
         rc("close #31")
         rc("close #32")
         rc(
             "molmap #33 4 onGrid #0 modelId #34"
         )  # produces a map (resolution 4 A) from the combined model
-        rc(f"volume #34 save {path}{new_name}.mrc")  # saves the map
+        rc("volume #34 save {}{}.mrc".format(path, new_name))  # saves the map
         rc("~select")
         rc("close #33")
         rc("close #34")
+
+#if __name__ == "__main__":
+# Read the txt file of the chimera commands in the format of the arrangements doc
+with open("path/chimera_commands.txt") as f:
+    lines = f.readlines()
+# the first line is ignored and there is a blank line between any two entries
+Bfr1_lines = lines[1::3]
+Bfr2_lines = lines[2::3]
+basename = "simulated"  # set a base name as you want
+path = "./chimera_simulations/"
+generate(Bfr1_lines, Bfr2_lines, basename, path)
