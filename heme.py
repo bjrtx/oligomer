fe4cb5736db763ebfb6a660ab5cb658daff86624
bjrtx <57685415+bjrtx@users.noreply.github.com>
2022-11-10 21:35:01 +0200

    update heme.py

--- heme.py
+++ heme.py
@@ -1,9 +1,10 @@
+import shutil
+import logging
+
 import skimage
 import mrcfile
 import numpy as np
 import hotspots
-import shutil
-import logging
 
 """
 This script is not intended to be run frequently. It contains a method for taking one MRC file containing
@@ -20,52 +21,57 @@ The script needs to be run in the folder containing
 The outputs will be named "chain_plus_heme_A.mrc" and so on, "dimer_plus_heme_AQ.mrc" and so on.
 """
 
+logging.basicConfig(level=logging.INFO)
+
 # small threshold to erase background noise when reading hemes
-threshold = 0.0001
+threshold = 0.00001
 
 # in  the next variable, '?' will range from A to X
 chain_file_name = "Bfr_molmap_chain?_res5.mrc"
 heme_filename = "Heme_rerefined_corrected.mrc"
+
 heme = hotspots.read_mrc(heme_filename)
+heme = np.where(heme > threshold, heme, 0) # thresholding the heme map
+
 if heme.dtype != np.float32:
     logging.warning(
         "Read a file with unexpected numerical data type: {heme_filename}, {heme.dtype}."
     )
 
-logging.info("size of all hemes after threshold", np.count_nonzero(heme > threshold))
+logging.info(f"Size of the thresholded heme map: {np.count_nonzero(heme)}")
 
-heme_components = skimage.measure.label(heme > threshold)
+heme_components = skimage.measure.label(heme.astype(bool))
+# There are 12 connected components (1-12) plus 0 for empty regions
 assert set(heme_components.flat) == set(range(13))
-# there are 12 connected components (1-12) plus 0 for empty regions
-
-# dimers = ["aq", "bo", "cv", "du", "ep", "fr", "gk", "hn", "is", "jt", "lw", "mx"]
 
 example_file = chain_file_name.replace("?", "A")
 
+# dimers = ["aq", "bo", "cv", "du", "ep", "fr", "gk", "hn", "is", "jt", "lw", "mx"]
 for ab in hotspots.dimer_names:
+    logging.info(f"Writing maps for the chains in {ab}.")
     chains = [
         hotspots.read_mrc(chain_file_name.replace("?", char.upper())) for char in ab
     ]
+    # There is exactly one nonzero label in the intersection of chains[0] and the heme.
+    label = max(labels := set(heme_components[chains[0].astype(bool)].flat))
+    assert labels == {0, label} and 1 <= label <= 12 # one connected component label and 0
 
-    intersection = heme_components[chains[0].astype(bool)]
-    assert len(set(intersection.flat)) == 2  # one connected component label and 0
-    label = intersection.max()
-
-    # Part of the heme where heme_components == label
     right_component = np.where(heme_components == label, heme, 0)
-    logging.info("Processing a heme of size {np.count_nonzero(right_component)}.")
+    logging.info(f"Processing a heme of size {np.count_nonzero(right_component)}.")
     # Add the heme component to the chain by taking maxima element-wise.
     aug_chains = [np.maximum(chain, right_component) for chain in chains]
     dimer = np.maximum(*aug_chains)
 
     for char, chain in zip(ab, aug_chains):
-        # preserve the headers
-        shutil.copyfile(example_file, f"chain_plus_heme_{char.upper()}.mrc")
-        with mrcfile.open(f"chain_plus_heme_{char.upper()}.mrc", "r+") as file:
+        # Copy an existing file to preserve the header
+        new_file = f"chain_plus_heme_{char.upper()}.mrc"
+        shutil.copyfile(example_file, new_file)
+        with mrcfile.open(new_file, "r+") as file:
             file.set_volume()  # chimera seems not to set its headers properly
             file.set_data(chain)
 
-    shutil.copyfile(example_file, f"dimer_plus_heme_{ab.upper()}.mrc")
-    with mrcfile.open(f"dimer_plus_heme_{ab.upper()}.mrc", "r+") as file:
+    new_file = f"dimer_plus_heme_{ab.upper()}.mrc"
+    shutil.copyfile(example_file, new_file)
+    with mrcfile.open(new_file, "r+") as file:
         file.set_volume()
         file.set_data(dimer)
