48b4ed0476a0c0d579edac68a46894051ff1665a
bjrtx <57685415+bjrtx@users.noreply.github.com>
2022-11-10 23:20:07 +0200

    update hotspots.py

--- hotspots.py
+++ hotspots.py
@@ -18,7 +18,7 @@ import learning
 def read_mrc(filename: str, dtype=None) -> np.ndarray:
     """
     Read an MRC map from a file and convert it into a Numpy multidimensional array.
-    The values are cast to the specified data type, by default 16-bit floats.
+    The values are cast to the specified data type if one is given.
     """
     with mrcfile.open(filename) as mrc:
         return mrc.data if dtype is None else mrc.data.astype(dtype)
@@ -38,25 +38,25 @@ def gloves(hotspot: dict) -> list[np.ndarray, np.ndarray]:
         except (FileNotFoundError, ValueError):
             # This happens if the CSV file does not have either bfr info or a threshold,
             # in which case the glove is identically zero.
-            logging.warning(f"One glove was empty: {hotspot}.")
+            logging.warning(f"One glove was empty: {hotspot}. This will create more warnings.")
     return gloves
 
 
-def biggest_blob(logical: "np.ndarray[bool]", number: int = 1) -> "np.ndarray[bool]":
+def biggest_blob(logical: "np.ndarray[bool]", n: int = 1) -> "np.ndarray[bool]":
     """
-    Given a logical multidimensional array, find the number largest connected regions
-    and return them as a logical array. By default number is 1.
-    If number is higher the elementwise maximum of the arrays is returned.
+    Given a logical multidimensional array, find the n largest connected regions
+    and return them as a logical array. By default n is 1.
+    If n is higher the elementwise maximum of the arrays is returned.
     If the input array is all-zero then so is the output.
     """
     labeled = skimage.measure.label(logical)
     regions = skimage.measure.regionprops(labeled)
 
-    biggest = heapq.nlargest(number, regions, key=lambda r: np.sum(r.image))
+    biggest = heapq.nlargest(n, regions, key=lambda r: np.sum(r.image))
     labels = [r.label for r in biggest]
-    if len(labels) < number:
+    if len(labels) < n:
         logging.warning(
-            f"Fewer connected components than expected ({len(labels)}/{number}), perhaps due to an empty glove."
+            f"Fewer connected components than expected ({len(labels)}/{n}), perhaps due to an empty glove."
         )
     return functools.reduce(np.logical_or, (labeled == l for l in labels), 0)
 
@@ -65,38 +65,38 @@ dimer_names = ["aq", "bo", "cv", "du", "ep", "fr", "gk", "hn", "is", "jt", "lw",
 
 
 def process(
-    hotspot_filename: str, map_filename: str, map_threshold: float, by_dimers=False
+    hotspot_filename: str, map_filename: str, by_dimers=False
 ):
     """
     Process a density map and return a 2-dimensional array (rows are hotspots,
     columns are chains, entries are scores). Alternatively, if by_dimers is True
     then the columns are dimers.
 
-    hotspot_filename: CSV file containing the hotspot information, must be saved
+    hotspot_filename: CSV file containing the hotspot information, _must_ be saved
         in the UTF8 encoding. It has at least the following named columns:
         - "Bfr1_file_name" and "Bfr2_file_name" contain the paths of MRC files,
         - "Bfr1_Molmap_TH" and "Bfr2_Molmap_TH" contain floating point numbers
           between 0 and 1 intended as thresholds.
 
     map_filename: MRC file containing an electronic density map.
-    map_threshold: this parameter seems unused.
     """
 
     logging.info(f"Processing {map_filename}. Hotspot information: {hotspot_filename}.")
 
-    with open(hotspot_filename) as hotspot_file:
+    with open(hotspot_filename, encoding="utf-8") as hotspot_file:
         hotspots = list(csv.DictReader(hotspot_file))
 
     chain_threshold = 0.42
-
+    chain_filename = "chain_plus_heme_?.mrc" #"Bfr_molmap_chain?_res5.mrc"
+    dimer_filename = "dimer_plus_heme_?.mrc"
     if not by_dimers:
         columns = [
-            read_mrc(f"Bfr_molmap_chain{char}_res5.mrc") > chain_threshold
+            read_mrc(chain_filename.replace('?', char)) > chain_threshold
             for char in string.ascii_uppercase[:24]
         ]
     else:
         columns = [
-            read_mrc(f"dimer_plus_heme_{d.upper()}.mrc") > chain_threshold
+            read_mrc(dimer_filename.replace('?', d.upper())) > chain_threshold
             for d in dimer_names
         ]
 
@@ -110,15 +110,15 @@ def process(
             # largest connected component of the hotspot-chain intersection
             # is kept.
             bfr1_spot = biggest_blob(
-                bfr1_glove & chain_or_dimer, number=2 if by_dimers else 1
+                bfr1_glove & chain_or_dimer, n=2 if by_dimers else 1
             )
             bfr2_spot = biggest_blob(
-                bfr2_glove & chain_or_dimer, number=2 if by_dimers else 1
+                bfr2_glove & chain_or_dimer, n=2 if by_dimers else 1
             )
             comb_size = np.count_nonzero(bfr1_spot ^ bfr2_spot)
             # The disjoint union is empty if the two domains are equal - very unlikely.
             if not comb_size:
-                output = 0.0
+                output = 0
                 logging.error(
                     """
                     A residue has two identical gloves - very unlikely.
@@ -139,8 +139,8 @@ def process(
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     map_filename = "284postprocess.mrc"
-    map_threshold = 0.04
     hotspot_filename = "bbRefinedHotSpotsListDaniel.csv"
-    out = process(hotspot_filename, map_filename, map_threshold, by_dimers=True)
+    out = process(hotspot_filename, map_filename, by_dimers=True)
     learning.analyze(out.transpose())
